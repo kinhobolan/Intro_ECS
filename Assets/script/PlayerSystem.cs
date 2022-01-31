7628d7c26073c771dc7917c33cd71b097b00ce8b
Giácomo Antônio Althoff Bolan <kinhobolan@live.com>
2022-01-31 09:26:23 -0300

    Add all Base Methods

--- Assets/script/PlayerSystem.cs
+++ Assets/script/PlayerSystem.cs
@@ -3,16 +3,37 @@ using UnityEngine;
 
 public class PlayerSystem : SystemBase {
 
+    protected override void OnCreateForCompiler () {
+
+        Debug.Log ("OnCreateForCompiler()");
+    }
+
+
     protected override void OnCreate () {
 
-        Debug.Log ("OnCreate");
+        Debug.Log ("OnCreate()");
     }
 
+    protected override void OnStartRunning () {
+
+        Debug.Log ("OnStartRunning() ");
+
+    }
+
+  
     protected override void OnUpdate () {
 
-
-        Debug.Log ("OnUpdate");
+        Debug.Log ("OnUpdate()");
     }
 
-    
+    protected override void OnStopRunning () {
+
+        Debug.Log ("OnStopRunning()");
+
+    }
+
+    protected override void OnDestroy () {
+
+        Debug.Log ("OnDestroy()");
+    }
 }
