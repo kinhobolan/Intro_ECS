68988663a292c3770d0fbd5847b686a94eed7712
Giácomo Antônio Althoff Bolan <kinhobolan@live.com>
2022-01-31 09:20:24 -0300

    Add Metodos Padroes

---
+++ Assets/script/PlayerData.cs
@@ -0,0 +1,7 @@
+using Unity.Entities;
+
+public struct PlayerData : IComponentData {
+
+    public int life;
+
+}
---
+++ Assets/script/PlayerSystem.cs
@@ -0,0 +1,18 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerSystem : SystemBase {
+
+    protected override void OnCreate () {
+
+        Debug.Log ("OnCreate");
+    }
+
+    protected override void OnUpdate () {
+
+
+        Debug.Log ("OnUpdate");
+    }
+
+    
+}
